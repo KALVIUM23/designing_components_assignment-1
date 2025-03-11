818386fad54bbba2bba2955986a3e8e8df706423
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-03-11 13:28:26 +0530

    assignment completed

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,26 @@
 // write the component code here
+import React from 'react'
+import './usercard.css'
+
+const Usercard = () => {  
+    const data = { 
+        photourl:"https://res.cloudinary.com/dabeupfqq/image/upload/v1736155511/uploads/umfvwghnqwtg0whlvcms.jpg", 
+        name:"P Sagar", 
+        email:"pitta.sagar.s81@kalvium.community", 
+        phoneNumber:"3345665352", 
+        address: "Bangalore, India" 
+    }
+    return (  
+      <div className="usercard">  
+        <img src={data.photourl} alt={data.name} className="usercard-photo" />  
+        <div className="usercard-details">  
+          <h2 className="usercard-name">{data.name}</h2>  
+          <p className="usercard-email">{data.email}</p>  
+          <p className="usercard-phone">{data.phoneNumber}</p>  
+          <p className="usercard-address">{data.address}</p>  
+        </div>  
+      </div>  
+    );  
+};  
+
+export default Usercard
